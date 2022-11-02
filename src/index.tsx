959dde999aae9c930485dd1e460c70c9407f2ce2
Michal <michal.benoo@gmail.com>
2022-11-02 23:45:51 +0100

    Revert changes to main index.html

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import MainPage from "./pages/MainPage";
 import "./assets/css/index.css";
 
-ReactDOM.createRoot(document.body as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <MainPage />
     </React.StrictMode>
