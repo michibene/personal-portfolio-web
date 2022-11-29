6069891f0d535894ea16a9019b70d11773e89d48
Michal <michal.benoo@gmail.com>
2022-11-29 09:09:23 +0100

    Tidy up assets in build/dist to separate folders

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,26 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    build: {
+        rollupOptions: {
+            output: {
+                chunkFileNames: "assets/js/[name]-[hash].js",
+                entryFileNames: "assets/js/[name]-[hash].js",
+
+                assetFileNames: ({ name }) => {
+                    if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? "")) {
+                        return "assets/images/[name]-[hash][extname]";
+                    }
+
+                    if (/\.css$/.test(name ?? "")) {
+                        return "assets/css/[name]-[hash][extname]";
+                    }
+
+                    // default value
+                    // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+                    return "assets/[name]-[hash][extname]";
+                },
+            },
+        },
+    },
 });
