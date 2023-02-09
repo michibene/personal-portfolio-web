be585e8eaa940a7e9c8798f831d8a7c1d7805326
Michal <michal.benoo@gmail.com>
2023-02-10 00:03:46 +0100

    Add unknown error page

--- src/Web.tsx
+++ src/Web.tsx
@@ -3,7 +3,8 @@ import portfolioWorksImagesUrls from "data/developer/cachedPortfolioImagesUrls";
 import developerPortfolio from "data/developer/developerPortfolio";
 import GFADashboardApp from "pages/developer/works/GFADashboardApp";
 import HEBEAVirtualQueueApp from "pages/developer/works/HEBEAVirtualQueueApp";
-import Error404Page from "pages/Error404Page";
+import Error404Page from "pages/error-pages/Error404Page";
+import UnknownErrorPage from "pages/error-pages/UnknownErrorPage";
 import LandingPage from "pages/LandingPage";
 import PreloadingPage from "pages/PreloadingPage";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
@@ -24,7 +25,7 @@ export default function Web() {
 
     const router = createBrowserRouter(
         createRoutesFromElements(
-            <Route path="/">
+            <Route path="/" errorElement={<UnknownErrorPage />}>
                 <Route index element={<LandingPage />} />
                 <Route path={getPathLinkById(1)} element={<HEBEAVirtualQueueApp />} />
                 <Route path={getPathLinkById(2)} element={<GFADashboardApp />} />
